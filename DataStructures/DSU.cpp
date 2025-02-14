6adab4405813394f78a8d044cc4220e0cf3b09b7
CodeHun7er <codehun7er@gmail.com>
2025-02-14 21:42:03 +0300

    DSU added

---
+++ DataStructures/DSU.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct DSU {
+  size_t n;
+  vector<size_t> parent;
+  vector<size_t> rank;
+
+  explicit DSU(size_t n) {
+    this->n = n;
+    this->parent.assign(n, 0);
+    this->rank.assign(n, 0);
+
+    for (size_t i = 0; i < n; ++i) {
+      this->parent[i] = i;
+    }
+  }
+
+  size_t leader(size_t v) {
+    if (this->parent[v] == v) {
+      return v;
+    }
+
+    this->parent[v] = leader(this->parent[v]);
+    return this->parent[v];
+  }
+
+  void unite(size_t a, size_t b) {
+    a = leader(a);
+    b = leader(b);
+
+    if (a == b) {
+      return;
+    }
+
+    if (this->rank[a] > this->rank[b]) {
+      swap(a,b);
+    }
+
+    this->parent[a] = b;
+    this->rank[b] = max(this->rank[a] + 1, this->rank[b]);
+  }
+};
